9fa5969672523b57906946bd43b473d771e5a3ed
Bahriddin0105 <87873365+Bahriddin0105@users.noreply.github.com>
2021-09-17 10:51:08 +0300

    Bahriddin

--- src/panel/Login.js
+++ src/panel/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Row, Col, Alert, Container , Form ,Button } from "react-bootstrap";
 import {Link} from 'react-router-dom'
 import fox from "./fox.jpg";
-import "./Login.css";
+import "../Login.css";
 export default class RouterA extends Component {
   render() {
     return (
@@ -11,23 +11,45 @@ export default class RouterA extends Component {
           <Row>
             <Col xs={6} style={{ padding: "20px", boxSizing: "borderBox", marginTop: '8%' }}>
               <Container>
+                <Container>
                 {" "}
-                <Alert variant={"secondary"} style={{ textAlign: "center" }}>
+                
+                
+                
+                
+                
+                
+                <Alert variant={"secondary"} style={{ textAlign: "center", borderRadius:'15px' }}>
                   Login va parolni kiriting
                 </Alert><br/>
                 <Form>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Group className="mb-3" controlId="formBasicLogin">
                     <Form.Label>Login</Form.Label>
-                    <Form.Control className='inp' type="word" placeholder="Enter login" />
+                    <Form.Control className='inp' style={{borderRadius:'15px'}} type="word" placeholder="Enter login" />
                   </Form.Group>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Parol</Form.Label>
-                    <Form.Control className='inp' type="password" placeholder="Enter password" />
+                    <Form.Control className='inp' style={{borderRadius:'15px'}} type="password" placeholder="Enter password" />
                   </Form.Group>
 
                   
                 </Form>
-                <Link to='/adminpanel/bosh'><Button variant="primary" style={{width: '100%', marginTop: '20px'}}>KIRISH</Button></Link>
+
+
+
+
+
+
+
+
+                <Link to='/adminpanel/bosh'><Button variant="primary" className='btn' style={{width: '100%', marginTop: '20px', borderRadius:'15px'}}>KIRISH</Button></Link>
+
+
+
+
+
+
+                </Container>
               </Container>
             </Col>
             <Col xs={6}>
