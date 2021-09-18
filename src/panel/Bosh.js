4364aba708b1d238469b189fcf817522c7145239
Bahriddin0105 <87873365+Bahriddin0105@users.noreply.github.com>
2021-09-18 17:47:33 +0500

    .

--- src/panel/Bosh.js
+++ src/panel/Bosh.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Modal, Button } from "antd";
-import { Form } from "react-bootstrap";
+import { Form, FloatingLabel} from "react-bootstrap";
 import "./Login.css";
 export default class Bosh extends Component {
   state = {
@@ -29,19 +29,18 @@ export default class Bosh extends Component {
     return (
       <div>
         <Button type="primary" onClick={this.showModal}>
-          Dorilarni kiritish
+          Malumotlarni kiritish
         </Button>
         <Modal
           visible={visible}
-          title="Dorilarni ma'lumotlarini kiritish"
+          title="Ma'lumotlarini kiritish"
           onOk={this.handleOk}
           centered
           className="Modal"
-          
           onCancel={this.handleCancel}
           footer={[
             <Button key="back" onClick={this.handleCancel}>
-              Return
+              Cancel
             </Button>,
             <Button
               key="submit"
@@ -53,50 +52,95 @@ export default class Bosh extends Component {
             </Button>,
           ]}
         >
-          <div style={{height:'400px', overflowY:'auto'}}><Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Nomi</Form.Label>
-              <Form.Control type="text" placeholder="Nomini kiriting" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-              <Form.Label>Ma'lumot</Form.Label>
-              <Form.Control type="text" placeholder="Ma'lumot kiriting" />
-            </Form.Group>
-            <Form.Group controlId="formFile" className="mb-3">
+          <div style={{ height: "400px", overflowY: "auto" }}>
+          
+            <Form>
+            
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput">
+                <Form.Label>Video</Form.Label>
+                <Form.Control type="text" placeholder="Video" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Manzil</Form.Label>
+                <Form.Control type="text" placeholder="Manzilni kiriting" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput2"
+              >
+                <Form.Label>Telefon raqam</Form.Label>
+                <Form.Control
+                  type="tel"
+                  placeholder="Telefon raqamni kiriting"
+                />
+              </Form.Group>
+              {/* <Form.Group controlId="formFile" className="mb-3">
               <Form.Label>Rasmni kiriting</Form.Label>
               <br />
               <Form.Control type="file" placeholder="rasmni kiriting" />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Tarkibi</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
-              <Form.Label>Kasallik</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Kasallik turini kiriting"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
-              <Form.Label>Saqlanishi</Form.Label>
-              <Form.Control type="text" placeholder="Saqlanishini kiriting" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
-              <Form.Label>Qo'llanish</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Qo'llanish usulini kiriting"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput6">
-              <Form.Label>Narxi</Form.Label>
-              <Form.Control type="number" placeholder="Narxini kiriting" />
-            </Form.Group>
-          </Form></div>
+            </Form.Group> */}
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput3"
+              >
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Email manzilingizni kiriting"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput4"
+              >
+                <Form.Label>Instagram manzil</Form.Label>
+                <Form.Control type="text" placeholder="Instagram manzil" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput5"
+              >
+                <Form.Label>Telegram manzil</Form.Label>
+                <Form.Control type="text" placeholder="Telegram manzil" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput6"
+              >
+                <Form.Label>Facebook manzil</Form.Label>
+                <Form.Control type="text" placeholder="Facebook manzil" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput7"
+              >
+                <Form.Label>You tube</Form.Label>
+                <Form.Control type="number" placeholder="You tube" />
+              </Form.Group>
+              <p>Ishni boshlanish kuni</p>
+              <select className="select" aria-selected="Dushanba">
+                <option value="1">Dushanba</option>
+                <option value="2">Seshanba</option>
+                <option value="3">Chorshanba</option>
+                <option value="4">Payshanba</option>
+                <option value="5">Juma</option>
+                <option value="6">Shanba</option>
+              </select>
+              <br />
+              <p>Ishning tugash kuni</p>
+              <select className="select" aria-selected="Dushanba">
+                <option value="1">Dushanba</option>
+                <option value="2">Seshanba</option>
+                <option value="3">Chorshanba</option>
+                <option value="4">Payshanba</option>
+                <option value="5">Juma</option>
+                <option value="6">Shanba</option>
+              </select>
+            </Form>
+          </div>
         </Modal>
       </div>
     );
